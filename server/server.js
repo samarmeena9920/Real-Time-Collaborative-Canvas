accca7d6161647ab198ddfd8263a62eb37faa83c
samarmeena9920 <150104404+samarmeena9920@users.noreply.github.com>
2025-11-13 03:39:22 +0530

    Refactor static file serving in server.js
    Updated static file serving to remove commented line.

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const io = new Server(server, { cors: { origin: '*' } });
 const PORT = process.env.PORT || 3000;
 
 // Serve static client
-app.use('/client', express.static(path.join(__dirname, '..', 'client')));
+//app.use('/client', express.static(path.join(__dirname, '..', 'client')));
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
